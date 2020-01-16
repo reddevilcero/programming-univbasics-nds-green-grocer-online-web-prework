fa73ff030a41fea7d6e74b0ef43e22e2057b63b3
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-16 20:03:51 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,16 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+
+  item = nil
+
+  collection.each { |element|
+    if element[:item] == name
+      item = element
+    end
+   }
+
+  item
 end
 
 def consolidate_cart(cart)
@@ -9,18 +19,62 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  new_cart = []
+
+  cart.each { |element|
+
+    new_item = find_item_by_name_in_collection(element[:item], new_cart)
+
+    if new_item
+      new_item[:count] +=1
+    else
+      element[:count] = 1
+      new_cart << element
+    end
+   }
+
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each { |item_with_coupon|
+    found_item = find_item_by_name_in_collection(item_with_coupon[:item], cart)
+    if found_item
+      new_item = {
+        item: "#{item_with_coupon[:item]} W/COUPON",
+        count: item_with_coupon[:num],
+        price: item_with_coupon[:cost] / item_with_coupon[:num],
+        clearance: found_item[:clearance]
+      }
+      cart << new_item
+      found_item[:count] -= new_item[:count]
+    else
+      new_item = {
+        item: "#{item_with_coupon[:item]} W/COUPON",
+        count: item_with_coupon[:num],
+        price: item_with_coupon[:cost] / item_with_coupon[:num],
+        clearance: false
+      }
+    end
+  }
+
+ cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+
+  cart.each { |item|
+    if item[:clearance]
+      item[:price] -= (item[:price] * 0.2).round(2)
+    end
+   }
+   cart
 end
 
 def checkout(cart, coupons)
@@ -33,4 +87,18 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  total = 0
+ p cart
+ p coupons
+  consolidate_cart = consolidate_cart(cart)
+  cart_with_coupons_applied = apply_coupons(consolidate_cart, coupons)
+  final_cart = apply_clearance(cart_with_coupons_applied)
+  p final_cart
+
+  final_cart.each { |element|
+
+    total+= element[:price] * element[:count]
+    }
+
+  total > 100 ? total -= (total * 0.1).round : total
 end
